51d76eff88bf8b5979539eb156f32cfdce248b00
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-14 21:49:47 +0200

    Add files via upload

---
+++ Joc.cpp
@@ -0,0 +1,26 @@
+#include "Joc.h"
+
+void Joc::inicialitza(const string& nomFitxer)
+{
+
+}
+
+bool Joc::giraFigura(DireccioGir direccio)
+{
+	return true;
+}
+
+bool Joc::mouFigura(int dirX)
+{
+	return true;
+}
+
+int Joc::baixaFigura()
+{
+	return 0;
+}
+
+void Joc::escriuTauler(const string& nomFitxer)
+{
+
+}
