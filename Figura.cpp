8d67d9f33bdf93a9652580b47b8ccb328f0384f2
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-15 12:23:59 +0200

    figura-constructor

--- Figura.cpp
+++ Figura.cpp
@@ -1,2 +1,15 @@
 #include "Figura.h"
 
+Figura::Figura()
+{
+    m_tipus = NO_FIGURA;
+    m_fila = -1;
+    m_columna = -1;
+    m_gir = 0;
+    m_color = NO_COLOR;
+    for (int i = 0; i < MAX_ALCADA; i++)
+    {
+        for (int j = 0; j < MAX_AMPLADA; j++)
+            m_figura[i][j] = m_tipus;
+    }
+}
