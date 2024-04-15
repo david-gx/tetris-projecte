177eafeee7f8798d58170f30c4e43e6ca22a2a2c
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-15 12:21:46 +0200

    classe-tauler-v1

--- Tauler.h
+++ Tauler.h
@@ -7,6 +7,13 @@ const int MAX_COL = 8;
 
 class Tauler
 {
+public:
+    Tauler();
+    void cercaEliminaFiles();
+    
+private:
+    void eliminaFila(const int& fila);
+    int m_tauler[MAX_FILA][MAX_COL];
 };
 
 #endif
