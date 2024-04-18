2482b0e942bb8d77d22064c6252e64b578832691
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-19 01:28:25 +0200

    inicialitzacio-Figura.cpp

--- Figura.cpp
+++ Figura.cpp
@@ -14,6 +14,138 @@ Figura::Figura()
     }
 }
 
+void Figura::inicialitzaFigura()
+{
+    switch (m_tipus)
+    {
+        case FIGURA_O:
+            m_nFiles = 2;
+            m_nColumnes = 2;
+            m_color = COLOR_GROC;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                    m_figura[i][j] = m_color;
+            }
+            m_centreFila = 0;
+            m_centreColumna = 0;
+            break;
+        
+        case FIGURA_I:
+            m_nFiles = 4;
+            m_nColumnes = 4;
+            m_color = COLOR_BLAUCEL;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                {
+                    if (i == 1)
+                        m_figura[i][j] = m_color;
+                    else
+                        m_figura[i][j] = NO_COLOR;
+                }
+            }
+            m_centreFila = 1;
+            m_centreColumna = 2;
+            break;
+        
+        case FIGURA_T:
+            m_nFiles = 3;
+            m_nColumnes = 3;
+            m_color = COLOR_MAGENTA;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                {
+                    if (i == 1 || i == 0 && j == 1)
+                        m_figura[i][j] = m_color;
+                    else
+                        m_figura[i][j] = NO_COLOR;
+                }
+            }
+            m_centreFila = 1;
+            m_centreColumna = 1;
+            break;
+        
+        case FIGURA_L:
+            m_nFiles = 3;
+            m_nColumnes = 3;
+            m_color = COLOR_TARONJA;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                {
+                    if (i == 1 || i == 0 && j == 2)
+                        m_figura[i][j] = m_color;
+                    else
+                        m_figura[i][j] = NO_COLOR;
+                }
+            }
+            m_centreFila = 1;
+            m_centreColumna = 1;
+            break;
+        
+        case FIGURA_J:
+            m_nFiles = 3;
+            m_nColumnes = 3;
+            m_color = COLOR_BLAUFOSC;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                {
+                    if (i == 1 || i == 0 && j == 0)
+                        m_figura[i][j] = m_color;
+                    else
+                        m_figura[i][j] = NO_COLOR;
+                }
+            }
+            m_centreFila = 1;
+            m_centreColumna = 1;
+            break;
+        
+        case FIGURA_Z:
+            m_nFiles = 3;
+            m_nColumnes = 3;
+            m_color = COLOR_VERMELL;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                {
+                    if (i == 0 && (j == 0 || j == 1) || i == 1 && (j == 1 || j == 2))
+                        m_figura[i][j] == m_color;
+                    else
+                        m_figura[i][j] = NO_COLOR;
+                }
+            }
+            m_centreFila = 1;
+            m_centreColumna = 1;
+        
+        case FIGURA_S:
+            m_nFiles = 3;
+            m_nColumnes = 3;
+            m_color = COLOR_VERD;
+            for (int i = 0; i < m_nFiles; i++)
+            {
+                for (int j = 0; j < m_nColumnes; j++)
+                {
+                    if (i == 0 && (j == 1 || j == 2) || i == 1 && (j == 0 || j == 1))
+                        m_figura[i][j] = m_color;
+                    else
+                        m_figura[i][j] = NO_COLOR;
+                }
+            }
+            m_centreFila = 1;
+            m_centreColumna = 1;
+            break;
+        
+        default:
+            break;
+    }
+    
+    for (int i = 0; i < m_gir; i++)
+        girHorari();
+}
+
 void Figura::transposada()
 {
     int aux;
