4bdf0d95f82b941a4f2651a0beca060518ef2139
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-18 23:43:52 +0200

    input-output-Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -5,7 +5,7 @@ Tauler::Tauler()
     for (int i = 0; i < MAX_FILA; i++)
     {
         for (int j = 0; j < MAX_COL; j++)
-            m_tauler[i][j] = 0;
+            m_tauler[i][j] = COLOR_NEGRE;
     }
 }
 
@@ -16,7 +16,7 @@ void Tauler::eliminaFila(const int& fila)
         for (int j = fila; j < MAX_FILA; j++)
             m_tauler[j][i] = m_tauler[j + 1][i];
         
-        m_tauler[MAX_FILA - 1][i] = 0;
+        m_tauler[MAX_FILA - 1][i] = COLOR_NEGRE;
     }
 }
 
@@ -29,7 +29,7 @@ void Tauler::cercaEliminaFiles()
     {
         while (j < MAX_COL && elimina)
         {
-            if (m_tauler[i][j] != 0)
+            if (m_tauler[i][j] != COLOR_NEGRE)
                  j++;
             else
                 elimina = false;
@@ -43,4 +43,35 @@ void Tauler::cercaEliminaFiles()
         else
             i--;
     }
+}
+
+ifstream& operator>>(ifstream& input, Tauler& tauler)
+{
+    int valor;
+    ColorFigura valorColor;
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        for (int j = 0; j < MAX_COL; j++)
+        {
+            input >> valor;
+            valorColor = (ColorFigura)valor;
+            tauler.setTauler(valorColor, i, j);
+            cout << valorColor;
+        }
+    }
+    return input;
+}
+
+ofstream& operator<<(ofstream& output, Tauler& tauler)
+{
+    int posicio;
+    for (int i = 0; i < MAX_FILA; i++)
+    {
+        for (int j = 0; j < MAX_COL; j++)
+        {
+            posicio = (int)tauler.getTauler(i, j);
+            output << posicio;
+        }
+    }
+    return output;
 }
