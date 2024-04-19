2f79749547f78131662a8759765c22695656020d
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-19 02:54:41 +0200

    funcions-inicials-Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -2,25 +2,37 @@
 
 void Joc::inicialitza(const string& nomFitxer)
 {
-
+    ifstream fitxerIn;
+    string nomFitxerIn;
+    cin >> nomFitxerIn;
+    fitxerIn.open(nomFitxerIn);
+    fitxerIn >> m_figuraJoc >> m_taulerJoc;
+    fitxerIn.close();
+    
+    m_taulerJoc.introdueixFigura(m_figuraJoc);
 }
 
 bool Joc::giraFigura(DireccioGir direccio)
 {
-	return true;
+    return false;
 }
 
 bool Joc::mouFigura(int dirX)
 {
-	return true;
+    return false;
 }
 
 int Joc::baixaFigura()
 {
-	return 0;
+    return 0;
 }
 
 void Joc::escriuTauler(const string& nomFitxer)
 {
-
+    ofstream fitxerOut;
+    string nomFitxerOut;
+    cin >> nomFitxerOut;
+    fitxerOut.open(nomFitxerOut);
+    fitxerOut << m_taulerJoc;
+    fitxerOut.close();
 }
