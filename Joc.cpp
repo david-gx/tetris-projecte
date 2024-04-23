d1f455938881f2060b272f73034b7cc477800475
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-23 22:43:51 +0200

    posicioValida-Joc.cpp

--- Joc.cpp
+++ Joc.cpp
@@ -1,14 +1,33 @@
 #include "Joc.h"
 
-void Joc::inicialitza(const string& nomFitxer)
+bool Joc::posicioValida(const Tauler& t, const Figura& f) const
 {
+    int maxFiles = f.getNFiles();
+    int maxColumnes = f.getNColumnes();
+    int fila = f.getFila();
+    int columna = f.getColumna();
+    int centreX = f.getCentreFila();
+    int centreY = f.getCentreColumna();
+    for (int i = 0; i < maxFiles; i++)
+    {
+        for (int j = 0; j < maxColumnes; j++)
+        {
+            if (f.getFigura(i, j) != NO_COLOR && t.getTauler(fila + i - centreX, columna + i - centreY) != COLOR_NEGRE)
+                return false;
+        }
+    }
+    return true;
+}
+
+void Joc::inicialitza(const string& nomFitxer)
+{/*
     ifstream fitxer;
     fitxer.open(nomFitxer);
     if (fitxer.is_open())
         fitxer >> m_figuraJoc >> m_taulerJoc;
     fitxer.close();
     
-    m_taulerJoc.introdueixFigura(m_figuraJoc);
+    m_taulerJoc.introdueixFigura(m_figuraJoc);*/
 }
 
 bool Joc::giraFigura(DireccioGir direccio)
