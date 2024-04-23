47d4aa4adf5c2ac6866a6d74c62fe1c53d6fd268
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-23 23:15:45 +0200

    canvi-introdueixFigura-Tauler.cpp

--- Tauler.cpp
+++ Tauler.cpp
@@ -20,8 +20,10 @@ void Tauler::introdueixFigura(const Figura& figura)
             m_taulerFigura[i][j] = m_tauler[i][j];
     }
     
-    int nFiles = figura.getNFiles(), nColumnes = figura.getNColumnes();
-    int xInicial = figura.getFila() - figura.getCentreFila(), yInicial = figura.getColumna() - figura.getCentreColumna();
+    int nFiles = figura.getNFiles()
+    int nColumnes = figura.getNColumnes();
+    int xInicial = figura.getFila() - figura.getCentreFila()
+    int yInicial = figura.getColumna() - figura.getCentreColumna();
     for (int i = xInicial; i < nFiles; i++)
     {
         for (int j = yInicial; j < nColumnes; j++)
@@ -30,7 +32,6 @@ void Tauler::introdueixFigura(const Figura& figura)
                 m_taulerFigura[i][j] = figura.getFigura(i - xInicial, j - yInicial);
         }
     }
-    
 }
 
 void Tauler::eliminaFila(const int& fila)
