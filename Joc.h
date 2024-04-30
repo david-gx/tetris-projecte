b45a2b6f0c17f9e700f93fa20108babef51963d7
david-gx <166950691+david-gx@users.noreply.github.com>
2024-04-30 16:29:49 +0200

    canvi-posicioValida-Joc.h

--- Joc.h
+++ Joc.h
@@ -15,7 +15,7 @@ public:
 	void escriuTauler(const string& nomFitxer);
     
 private:
-	bool posicioValida(const Tauler& t, const Figura& f) const;
+	bool posicioValida(const Figura& f) const;
     
 	Tauler m_taulerJoc;
 	Figura m_figuraJoc;
