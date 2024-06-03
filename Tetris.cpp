56dedb06068ed9a41f9050baefe1acbbfb052aeb
david-gx <166950691+david-gx@users.noreply.github.com>
2024-06-03 22:32:04 +0200

    lgrafica-v1-Tetris.cpp

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,157 +1,65 @@
-#include "Tauler.h"
+#include "Tetris.h"
 
-/**
-* Tauler
-* Constructor per defecte de la classe Tauler. Inicialitza totes les posicions del tauler a COLOR_NEGRE
-*/
-Tauler::Tauler()
-{
-    for (int i = 0; i < MAX_FILA; i++)
+void Tetris::juga(Screen& pantalla)
+{   
+    //Mostrem la finestra grafica
+    m_partida.inicialitzaPartida();
+    pantalla.show();
+    
+    bool partidaEnCurs = true;
+
+    Uint64 NOW = SDL_GetPerformanceCounter();
+    Uint64 LAST = 0;
+    double deltaTime = 0;
+
+    do
     {
-        for (int j = 0; j < MAX_COL; j++)
-        {
-            m_tauler[i][j] = COLOR_NEGRE;
-            m_taulerFigura[i][j] = COLOR_NEGRE;
-        }
-    }
+        LAST = NOW;
+        NOW = SDL_GetPerformanceCounter();
+        deltaTime = (double)((NOW - LAST) / (double)SDL_GetPerformanceFrequency());
+
+        // Captura tots els events de ratolí i teclat de l'ultim cicle
+        pantalla.processEvents();
+
+        partidaEnCurs = m_partida.actualitza(deltaTime);
+
+        // Actualitza la pantalla
+        if (partidaEnCurs)
+            pantalla.update();
+
+    } while (!Keyboard_GetKeyTrg(KEYBOARD_ESCAPE) && partidaEnCurs);
+    // Sortim del bucle si pressionem ESC o si finalitza la partida
+    
+    cout << "Introdueix el teu nom: ";
+    string nom;
+    cin >> nom;
+    afegeixPuntuacio(nom, m_partida.getPuntuacio());
 }
 
-/**
-* introdueixFigura
-* Funció que introdueix la figura passada com a paràmetre al tauler
-* @param figura: figura a introduir
-*/
-void Tauler::introdueixFigura(const Figura& figura)
+void Tetris::afegeixPuntuacio(const string& nom, const int& punts)
 {
-    // Copia en la matriu del tauler que es visualitzarà per pantalla el tauler amb les figures ja col·locades
-    for (int i = 0; i < MAX_FILA; i++)
-    {
-        for (int j = 0; j < MAX_COL; j++)
-            m_taulerFigura[i][j] = m_tauler[i][j];
-    }
+	std::forward_list<Puntuacio>::iterator anterior = m_puntuacions.before_begin();
+	std::forward_list<Puntuacio>::iterator actual = m_puntuacions.begin();
+	bool trobat = false;
 
-    // Afegeix en la matriu del tauler que es visualitzarà per pantalla la figura
-    int nFiles = figura.getNFiles();
-    int nColumnes = figura.getNColumnes();
-    int xInicial = figura.getFila() - figura.getCentreFila();
-    int yInicial = figura.getColumna() - figura.getCentreColumna();
-    for (int i = xInicial; i < nFiles + xInicial; i++)
-    {
-        for (int j = yInicial; j < nColumnes + yInicial; j++)
-        {
-            if (figura.getFigura(i - xInicial, j - yInicial) != NO_COLOR)
-                m_taulerFigura[i][j] = figura.getFigura(i - xInicial, j - yInicial);
-        }
-    }
+	while (!trobat && actual != m_puntuacions.end())
+	{
+		if ((*actual).getPunts() < punts)
+			trobat = true;
+		else
+		{
+			anterior = actual;
+			actual++;
+		}
+	}
+
+	Puntuacio puntuacio(nom, punts);
+	m_puntuacions.insert_after(anterior, puntuacio);
 }
 
-/**
-* actualitzaTauler
-* Funció que actualitza el tauler, de tal manera que, si hi havia una figura en moviment, quedarà fixa
-*/
-void Tauler::actualitzaTauler()
+void Tetris::mostraPuntuacions()
 {
-    for (int i = 0; i < MAX_FILA; i++)
-    {
-        for (int j = 0; j < MAX_COL; j++)
-            m_tauler[i][j] = m_taulerFigura[i][j];
-    }
-}
-
-/**
-* eliminaFila
-* Funció que elimina una fila
-* @param fila: fila a eliminiar
-*/
-void Tauler::eliminaFila(const int& fila)
-{
-    for (int i = 0; i < MAX_COL; i++)
-    {
-        for (int j = fila; j > 0; j--)
-        {
-            m_tauler[j][i] = m_tauler[j - 1][i];
-            m_taulerFigura[j][i] = m_tauler[j - 1][i];
-        }
-        m_tauler[0][i] = COLOR_NEGRE;
-        m_taulerFigura[0][i] = COLOR_NEGRE;
-    }
-}
-
-/**
-* cercaEliminaFiles
-* Funció que elimina totes les files completades del tauler
-* @return Nombre de files eliminades
-*/
-int Tauler::cercaEliminaFiles()
-{
-    int nFilesEliminades = 0;
-    int i = MAX_FILA, j = 0;
-    int filaDalt = 0;
-    bool elimina = true;
-    while (i > filaDalt)
-    {
-        // Cerca si una fila està ocupada, començant per la d'abaix
-        elimina = true;
-        j = 0;
-        while (j < MAX_COL && elimina)
-        {
-            if (m_tauler[i][j] != COLOR_NEGRE)
-                j++;
-            else
-                elimina = false;
-        }
-
-        // Si la fila està ocupada, l'elimina
-        if (elimina)
-        {
-            eliminaFila(i);
-            filaDalt++;
-            nFilesEliminades++;
-        }
-        // Si la fila no està ocupada, es passa a la següent fila
-        else
-            i--;
-    }
-    return nFilesEliminades;
-}
-
-/**
-* operator>>
-* Sobrecàrrega de l'operador >>
-*/
-ifstream& operator>>(ifstream& input, Tauler& tauler)
-{
-    int valor;
-    ColorFigura valorColor;
-    for (int i = 0; i < MAX_FILA; i++)
-    {
-        for (int j = 0; j < MAX_COL; j++)
-        {
-            input >> valor;
-            valorColor = (ColorFigura)valor;
-            tauler.setTauler(valorColor, i, j);
-            tauler.setTaulerFigura(valorColor, i, j);
-        }
-    }
-    return input;
-}
-
-/**
-* operator<<
-* Sobrecàrrega de l'operador <<
-*/
-ofstream& operator<<(ofstream& output, Tauler& tauler)
-{
-    int posicio;
-    for (int i = 0; i < MAX_FILA; i++)
-    {
-        for (int j = 0; j < MAX_COL; j++)
-        {
-            posicio = (int)tauler.getTaulerFigura(i, j);
-            output << posicio << " ";
-        }
-        output << "\n";
-    }
-    return output;
+	for (std::forward_list<Puntuacio>::iterator actual = m_puntuacions.begin(); actual != m_puntuacions.end(); actual++)
+		cout << (*actual).getNom() << " " << (*actual).getPunts() << "\n";
 }
 
