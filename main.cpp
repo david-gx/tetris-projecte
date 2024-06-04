af17f6133eb7d62ec62fb51b815a4374c01e69e7
david-gx <166950691+david-gx@users.noreply.github.com>
2024-06-04 21:16:15 +0200

    definitiu-main.cpp

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 //Llibreria grafica
 #include "../Graphic Lib/libreria.h"
 #include "../Graphic Lib/NFont/NFont.h"
-#include <conio.h>      /* getch */ 
+#include <conio.h>      /* getch */
 
 #elif __APPLE__
 //Llibreria grafica
@@ -25,38 +25,62 @@
 
 #include "GraphicManager.h"
 #include "Tetris.h"
-#include "Joc.h"
 
 int main(int argc, const char* argv[])
 {
-    //Instruccions necesaries per poder incloure la llibreria i que trobi el main
     SDL_SetMainReady();
     SDL_Init(SDL_INIT_VIDEO);
-
-    //Inicialitza un objecte de la classe Screen que s'utilitza per gestionar la finestra grafica
     Screen pantalla(SCREEN_SIZE_X, SCREEN_SIZE_Y);
 
     Tetris tetris;
     bool final = false;
     int instruccio = 0;
 
+    string nomFitxerPuntuacions;
+    cout << "Introdueix el nom del fitxer on es guardaran les puntuacions: ";
+    cin >> nomFitxerPuntuacions;
+
+
+    ifstream fitxerIn;
+    fitxerIn.open(nomFitxerPuntuacions);
+    if (fitxerIn.is_open())
+    {
+        string nomAux;
+        int puntuacioAux;
+        while (!fitxerIn.eof())
+        {
+            fitxerIn >> nomAux >> puntuacioAux;
+            if (!fitxerIn.eof())
+                tetris.afegeixPuntuacio(nomAux, puntuacioAux);
+        }
+    }
+    fitxerIn.close();
+
     do
     {
-        cout << "MENU PRINCIPAL" << "\n";
+        cout << "\n" << "MENU PRINCIPAL" << "\n";
         cout << "============" << "\n";
         cout << "1. Joc en mode normal" << "\n";
         cout << "2. Joc en mode test" << "\n";
         cout << "3. Mostrar puntuacions" << "\n";
         cout << "4. Sortir" << "\n";
+        
+        string nomFitxerTauler, nomFitxerFigures, nomFitxerMoviments;
         cin >> instruccio;
-
         switch (instruccio)
         {
         case 1:
-            tetris.juga(pantalla);
+            tetris.juga(pantalla, nomFitxerPuntuacions);
             break;
 
-        case 2:
+        case 2:            
+            cout << "Nom del fitxer amb l'estat inicial del tauler: ";
+            cin >> nomFitxerTauler;
+            cout << "Nom del fitxer amb la sequencia de figures: ";
+            cin >> nomFitxerFigures;
+            cout << "Nom del fitxer amb la sequencia de moviments: ";
+            cin >> nomFitxerMoviments;
+            tetris.jugaModeTest(pantalla, nomFitxerTauler, nomFitxerFigures, nomFitxerMoviments);
             break;
 
         case 3:
@@ -74,8 +98,9 @@ int main(int argc, const char* argv[])
 
     } while (!final);
 
-    //Instruccio necesaria per alliberar els recursos de la llibreria 
     SDL_Quit();
 
+    tetris.guardaPuntuacions(nomFitxerPuntuacions);
+    
     return 0;
 }
