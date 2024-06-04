9f9b4d870e92c57fb73d91dd1f0d2e174190691f
david-gx <166950691+david-gx@users.noreply.github.com>
2024-06-04 21:03:42 +0200

    definitiu-Figura.h

--- Figura.h
+++ Figura.h
@@ -6,33 +6,6 @@ using namespace std;
 
 #include "InfoJoc.h"
 
-/*
-typedef enum
-{
-    COLOR_NEGRE = 0,
-    COLOR_GROC,
-    COLOR_BLAUCEL,
-    COLOR_MAGENTA,
-    COLOR_TARONJA,
-    COLOR_BLAUFOSC,
-    COLOR_VERMELL,
-    COLOR_VERD,
-    NO_COLOR
-} ColorFigura;
-
-
-typedef enum
-{
-    NO_FIGURA = 0,
-    FIGURA_O,
-    FIGURA_I,
-    FIGURA_T,
-    FIGURA_L,
-    FIGURA_J,
-    FIGURA_Z,
-    FIGURA_S,
-} TipusFigura;
-*/
 const int MAX_ALCADA = 4;
 const int MAX_AMPLADA = 4;
 
@@ -100,4 +73,3 @@ private:
 ifstream& operator>>(ifstream& input, Figura& figura);
 
 #endif
-
