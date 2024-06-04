4bc46278cce3a55548a1e88645947dbe97581e75
david-gx <166950691+david-gx@users.noreply.github.com>
2024-06-04 21:14:25 +0200

    definitiu-Tetris.h

--- Tetris.h
+++ Tetris.h
@@ -5,12 +5,19 @@
 #include "Puntuacio.h"
 #include "Partida.h"
 
+/**
+* CLASS Tetris
+* Classe que permet guardar les dades del joc Tetris
+* Les dades que conté són: un objecte de classe Partida i una llista amb les millors puntuacions
+* Conté mètodes per jugar en mode normal i mode test i per gestionar les puntuacions dels jugadors
+*/
 class Tetris
 {
 public:
-	void juga(Screen& pantalla);
+	void juga(Screen& pantalla, const string& nomFitxerPuntuacions);
 	void mostraPuntuacions();
 	void afegeixPuntuacio(const string& nom, const int& punts);
+	void guardaPuntuacions(const string& nomFitxerPuntuacions);
 
 	void jugaModeTest(Screen& pantalla, const string& nomFitxerTauler, const string& nomFitxerFigures, const string& nomFitxerMoviments);
 
