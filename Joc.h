ed57dbb78177ba1b128776a93ca60014f1c73816
david-gx <166950691+david-gx@users.noreply.github.com>
2024-06-04 21:10:23 +0200

    definitiu-Joc.h

--- Joc.h
+++ Joc.h
@@ -5,11 +5,10 @@
 #include <string>
 
 #include "GraphicManager.h"
-#include "InfoJoc.h"
 
 /**
 * CLASS Joc
-* Classe que permet guardar les dades d'una partida de tetris
+* Classe que permet guardar les dades de l'estat actual del joc
 * Les dades que conté són: tauler del joc i figura en joc
 * Conté mètodes per inicialitzar el tauler llegint el seu estat inicial des de fitxer; girar, moure i
 * baixar la figura en joc; i guardar l'estat del tauler en un fitxer
@@ -17,7 +16,8 @@
 class Joc
 {
 public:
-	Figura getFigura() const { return m_figuraJoc;  }
+	Figura getFigura() const { return m_figuraJoc; }
+	ColorFigura getTauler(int fila, int columna) const { return m_taulerJoc.getTaulerFigura(fila, columna); }
 
 	void setFigura(Figura f) { m_figuraJoc = f; }
 	void setTauler(Tauler t) { m_taulerJoc = t; }
@@ -32,6 +32,8 @@ public:
 	int colocaFigura();
 	bool generaFigura();
 	void dibuixaTauler() const;
+
+	bool generaFiguraModeTest(const int& tipus, const int& fila, const int& columna, const int& gir);
 
 private:
 	bool posicioValida(const Figura& f) const;
